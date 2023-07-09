f63739a83b4bd51a4761459421c85f4632d19791
kimyongwook98 <rladyddnr0209@naver.com>
2023-07-09 15:07:26 +0900

    add allowed_hosts

--- kywApp/apps.py
+++ kywApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AwsappConfig(AppConfig):
+class KywappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'awsApp'
+    name = 'kywApp'
--- kywProject/urls.py
+++ kywProject/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import awsApp.views
+import kywApp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', awsApp.views.index, name = 'index'),
+    path('', kywApp.views.index, name = 'index'),
 ]
